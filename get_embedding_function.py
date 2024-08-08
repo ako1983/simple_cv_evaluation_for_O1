208c5ddad777bc032f2023465c33ea1a603280ad
Ako <ako1983@users.noreply.github.com>
2024-08-08 01:22:04 -0700

    Update get_embedding_function.py
    minor edit

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,7 +1,6 @@
 # from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-
-
+from langchain_openai import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
 
 def get_embedding_function():
     embeddings = OpenAIEmbeddings()
